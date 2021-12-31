c0738e09464de0e151e25fb63b7527f3bd15ffc8
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-31 23:11:30 +0100

    fetch na trending dziala

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,24 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getApiData } from "../utils/apiCalls";
+import { getApiData } from "./../utils/apiCalls";
+import { TRENDING } from "./../utils/apiKey";
 
 const HomePage = () => {
   const [fetchedMovies, setFetchedMovies] = useState([]);
   const [error, setError] = useState(null);
 
-
   useEffect(() => {
-     getApiData(
-       "https://api.themoviedb.org/3/movie/550?api_key=4c868f688ccaa6211ab12d77fba2ebd8"
-     )
-       .then((data) => {
-         console.log("pobrano z Api:", data);
-         setFetchedMovies(data);
-       })
-       .catch((err) => {
-         console.log("moj log z error.name", err.name);
-         setError(err);
-       })
-       .finally(() => console.log("finally"));
-  }, [])
+    getApiData(TRENDING)
+      .then((data) => {
+        console.log("pobrano z Api:", data.results);
+        setFetchedMovies(data.results);
+      })
+      .catch((err) => {
+        console.log("moj log z error.name", err.name);
+        setError(err);
+      })
+      .finally(() => console.log("finally"));
+  }, []);
   return (
     <div>
       HomePage - strona domowa z listą popularnych filmów.
@@ -33,17 +31,24 @@ const HomePage = () => {
         <li>
           <Link to="movies/id">jakis film z id</Link>
         </li>
-        {/* {fetchedMovies.map((movie) => ( */}
-        {/* <li key={movie.id}>{movie.original_title}</li> */}
-        {/* ))} */}
-        {
+        {fetchedMovies.map((movie) => (
+         <li key={movie.id}>
+           <Link to={`movies/${movie.id}`}>
+        {movie.original_title}
+            </Link>
+
+        </li> 
+         ))}
+        {/* {
           <li key={fetchedMovies.id}>
-            <Link to={`movies/${fetchedMovies.id}`}>{fetchedMovies.original_title}</Link>
+            <Link to={`movies/${fetchedMovies.id}`}>
+              {fetchedMovies.original_title}
+            </Link>
           </li>
-        }
+        } */}
       </ul>
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
--- src/utils/apiKey.js
+++ src/utils/apiKey.js
@@ -1,5 +1,7 @@
 const KEY = "4c868f688ccaa6211ab12d77fba2ebd8";
+//  "https://api.themoviedb.org/3/movie/550?api_key=4c868f688ccaa6211ab12d77fba2ebd8"
+const JEDEN_FILM = `https://api.themoviedb.org/3/movie/550?api_key=${KEY}`;
 const COS = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=${KEY}`;
-const TRENDING = `https://api.themoviedb.org/3/TRENDING/movie/week?api_key=${KEY}`;
+const TRENDING = `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`;
 
-export { KEY, COS, TRENDING };
+export { KEY, COS, JEDEN_FILM, TRENDING };
